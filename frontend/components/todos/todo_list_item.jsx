60f42059e7edf7106d59748f0d3eee480615a8e9
Fwep <tarancacacho@gmail.com>
2019-02-06 00:07:55 -0800

    Add delete and done buttons

--- frontend/components/todos/todo_list_item.jsx
+++ frontend/components/todos/todo_list_item.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
 
-const TodoListItem = ({todo}) => (
-  <li>{todo.title}</li>
-);
+class TodoListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    
+  }
+  
+  render() {
+    const {removeTodo, todo} = this.props;
+    console.log
+    return (
+      <li>
+        <h3>{this.props.todo.title}</h3>
+        <button onClick={() => removeTodo(todo)}>Delete</button>
+        <button onClick={() => receiveTodo(todo)}>Done</button>
+      </li>
+    );
+  }
+}
 
 export default TodoListItem;
