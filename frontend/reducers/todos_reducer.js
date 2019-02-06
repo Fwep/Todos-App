7706b17a78f909aeb00271f99f13dd0802253af4
Fwep <tarancacacho@gmail.com>
2019-02-05 21:45:40 -0800

    Create root and todos reducer, and allTodos selector

---
+++ frontend/reducers/todos_reducer.js
@@ -0,0 +1,38 @@
+import { RECEIVE_TODOS, RECEIVE_TODO } from '../actions/todo_actions';
+import merge from 'lodash/merge';
+
+const initialState = {
+  1: {
+    id: 1,
+    title: 'Get a job',
+    body: 'at Google',
+    done: false
+  },
+  2: {
+    id: 2,
+    title: 'Cut hair',
+    body: 'Next week',
+    done: false
+  },
+};
+
+const todosReducer = (state = initialState, action) => {
+  Object.freeze(state);
+  let nextState = {};
+
+  switch(action.type) {
+    case RECEIVE_TODOS:
+      action.todos.forEach(todo  => {
+        nextState[todo.id] = todo; 
+      });
+      return nextState;
+    case RECEIVE_TODO:
+      nextState = merge({}, state);
+      nextState[action.todo.id] = action.todo;
+      return nextState;
+    default:
+      return state;
+  }
+};
+
+export default todosReducer;
