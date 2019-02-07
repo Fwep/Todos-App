5586571f39a330cbfa87c53de08554151e814cbe
Fwep <tarancacacho@gmail.com>
2019-02-06 21:14:59 -0800

    Add selector to filter steps by todo

--- frontend/reducers/selectors.js
+++ frontend/reducers/selectors.js
@@ -1,3 +1,8 @@
 export const allTodos = ({ todos }) => {
   return Object.keys(todos).map(id => todos[id]);
 };
+
+export function stepsByTodoId(state, todoId) {
+  return Object.values(state.steps)
+    .filter(step => step.todo_id = todoId);
+};
