c57210bdd0d5e98951e4a2fcd2304b3dbe768fbe
Fwep <tarancacacho@gmail.com>
2019-02-06 20:06:39 -0800

    Add functionality to toggle 'Done' status

--- frontend/components/todos/todo_list_item.jsx
+++ frontend/components/todos/todo_list_item.jsx
@@ -1,19 +1,33 @@
 import React from 'react';
+import merge from 'lodash/merge';
 
 class TodoListItem extends React.Component {
   constructor(props) {
     super(props);
     
+    this.state = this.props.todo;
+    this.toggleTodo = this.toggleTodo.bind(this);   
   }
   
+  toggleTodo(e) {
+    e.preventDefault();
+    const toggledTodo = merge(
+      {},
+      this.props.todo,
+      { done: !this.props.todo.done}
+    )
+
+    this.props.receiveTodo(toggledTodo);
+  }
+
   render() {
     const {removeTodo, todo} = this.props;
-    console.log
+    const { done } = todo; 
     return (
       <li>
         <h3>{this.props.todo.title}</h3>
         <button onClick={() => removeTodo(todo)}>Delete</button>
-        <button onClick={() => receiveTodo(todo)}>Done</button>
+        <button onClick={this.toggleTodo}>{done ? 'Undo' : 'Done'}</button>
       </li>
     );
   }
