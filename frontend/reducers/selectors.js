9425b590c6ea0d60e20c220cc6f4af44776f2875
Fwep <tarancacacho@gmail.com>
2019-02-09 00:02:26 -0800

    Fix assignment/equality typo

--- frontend/reducers/selectors.js
+++ frontend/reducers/selectors.js
@@ -2,7 +2,7 @@ export const allTodos = ({ todos }) => {
   return Object.keys(todos).map(id => todos[id]);
 };
 
-export function stepsByTodoId(state, todoId) {
+export const stepsByTodoId = (state, todoId) => {
   return Object.values(state.steps)
-    .filter(step => step.todo_id = todoId);
+    .filter(step => step.todo_id === todoId);
 };
