9cc32d32e3967e9a37f80dfbd9985cdaf143c2eb
Fwep <tarancacacho@gmail.com>
2019-02-09 00:01:18 -0800

    Render steps for each todo and button to delete todo

--- frontend/components/todos/todo_detail_view.jsx
+++ frontend/components/todos/todo_detail_view.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import StepListContainer from '../step_list/step_list_container';
 
 class TodoDetailView extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.deleteTodo = this.deleteTodo.bind(this);
+  }
+
+  deleteTodo() {
+    this.props.removeTodo(this.props.todo);
+  }
+  
   render() {
     return (
-      <h2>{this.props.body}</h2>
+      <div>
+        <h2>{this.props.body}</h2>
+        <StepListContainer todo_id={this.props.todo.id}/>
+        <button onClick={this.deleteTodo}>Delete Todo</button>
+      </div>
     );
   }
 }
