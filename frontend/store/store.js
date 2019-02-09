506aef5e969196c01c313a59dab1179004f59992
Fwep <tarancacacho@gmail.com>
2019-02-09 00:30:28 -0800

    Create state persistence via localStorage API

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -2,8 +2,15 @@ import { createStore } from 'redux';
 import rootReducer from '../reducers/root_reducer';
 
 const configureStore = () => {
- const store = createStore(rootReducer);
- return store;
+  const initialState = localStorage.state ? JSON.parse(localStorage.state) : {}
+  const store = createStore(rootReducer, initialState);
+
+  store.subscribe(() => {
+  let state = JSON.stringify(store.getState());
+  localStorage.state = state;
+  })
+
+  return store;
 }
 
 export default configureStore;
