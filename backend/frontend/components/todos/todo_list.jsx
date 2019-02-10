0f843251c5cc921c890c139df1bbebf7ef0f18f2
Fwep <tarancacacho@gmail.com>
2019-02-10 14:06:56 -0800

    Add createTodo and fetchTodos thunk action creators, and call upon mounting of TodoList

--- backend/frontend/components/todos/todo_list.jsx
+++ backend/frontend/components/todos/todo_list.jsx
@@ -2,26 +2,37 @@ import React from 'react';
 import TodoListItem from './todo_list_item';
 import TodoForm from './todo_form';
 
-const TodoList = ({todos, receiveTodo, removeTodo}) => {
-  const todoItems = todos.map(todo => {
-      return <TodoListItem 
-        key={`todo-list-item${todo.id}`} 
-        todo={todo} 
-        removeTodo={removeTodo}
-        receiveTodo={receiveTodo}
-      />
-    });
+class TodoList extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  
+  componentDidMount() {
+    console.log('Mounted');
+    this.props.fetchTodos();
+  }
 
-  return (
-    <div>
-      <ul>
-        {todoItems}
-      </ul>
-      <TodoForm 
-        receiveTodo={receiveTodo} 
-      />
-    </div>
-  );  
+  render() {
+    const todoItems = this.props.todos.map(todo => {
+    return <TodoListItem 
+    key={`todo-list-item${todo.id}`} 
+    todo={todo} 
+    removeTodo={this.props.removeTodo}
+    receiveTodo={this.props.receiveTodo}
+    />
+      });
+  
+    return (
+      <div>
+        <ul>
+          {todoItems}
+        </ul>
+        <TodoForm 
+          receiveTodo={this.props.receiveTodo} 
+        />
+      </div>
+    );  
+  }
 };
 
 export default TodoList;
--- backend/frontend/components/todos/todo_list_container.jsx
+++ backend/frontend/components/todos/todo_list_container.jsx
@@ -1,12 +1,15 @@
 import { connect } from 'react-redux';
 import TodoList from './todo_list';
 import { allTodos } from '../../reducers/selectors';
+import {fetchTodos, receiveTodo, removeTodo, createTodo} from '../../actions/todo_actions';
 
 const mSP = state => ({
   todos: allTodos(state)
 });
 
 const mDP = dispatch => ({
+  createTodo: (todo) => dispatch(createTodo(todo)),
+  fetchTodos: () => dispatch(fetchTodos()),
   receiveTodo: (todo) => dispatch(receiveTodo(todo)),
   removeTodo: (todo) => dispatch(removeTodo(todo))
 });
