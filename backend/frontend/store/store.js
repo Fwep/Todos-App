07bd525bc481ddc86362bd8b537f7c2a36b27118
Fwep <tarancacacho@gmail.com>
2019-02-10 14:05:50 -0800

    Apply custom thunk middleware

--- backend/frontend/store/store.js
+++ backend/frontend/store/store.js
@@ -1,9 +1,14 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers/root_reducer';
+import thunk from '../middleware/thunk';
 
 const configureStore = () => {
   const initialState = localStorage.state ? JSON.parse(localStorage.state) : {}
-  const store = createStore(rootReducer, initialState);
+  const store = createStore(
+    rootReducer, 
+    initialState, 
+    applyMiddleware(thunk));
+
 
   store.subscribe(() => {
   let state = JSON.stringify(store.getState());
